7b983c9d8cb59bce08752737a87a414e87f6aad5
alimakki1661 <alimakki1661@gmail.com>
2026-03-28 18:39:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ function loadFacts() {
     fetch("https://brianobruno.github.io/cats.json")
         .then(response => response.json())
         .then(data => {
-
-            // sort by factId
             const sortedFacts = data.facts.sort((a, b) => a.factId - b.factId);
 
-            // clear table before adding new rows
             const table = document.getElementById("factTable");
             table.innerHTML = "";
 
-            // populate table
             sortedFacts.forEach(item => {
                 table.innerHTML += `
                     <tr>
@@ -22,7 +18,6 @@ function loadFacts() {
                 `;
             });
 
-            // update image
             document.getElementById("catImage").src = data.catPhoto;
         })
         .catch(error => {
